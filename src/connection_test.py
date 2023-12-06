01703bee21e2e132283aa686b9349cf45c29cc54
Zewei Sun <ge89jem@mytum.de>
2023-12-06 14:57:02 +0100

    no need to use ros, add test_connection.py

---
+++ src/connection_test.py
@@ -0,0 +1,35 @@
+from task import *
+import time
+import math
+
+# doc sphinx
+
+def get_robot_state(robot="localhost"):
+    """Get the state of the robot.
+    :param robot:The hostname or IP address of the robot, defaults to "localhost"
+    :type robot: str, optional
+    :return: The state of the robot
+    :rtype: Any (replace with the actual type)
+    """
+    return call_method(robot, 12000, "get_state")
+
+
+def lock_robot(robot="localhost"):
+    """Lock the robot.
+
+    :param robot: The hostname or IP address of the robot, defaults to "localhost"
+    :type robot: str, optional
+    :return: The result of the lock operation
+    :rtype: Any (replace with the actual type)
+    """
+    return call_method(robot, 12000, "lock")
+
+def unlock_robot(robot="localhost"):
+    """Unlock the robot.
+
+    :param robot: The hostname or IP address of the robot, defaults to "localhost"
+    :type robot: str, optional
+    :return: The result of the unlock operation
+    :rtype: Any (replace with the actual type)
+    """
+    return call_method(robot, 12000, "unlock")
