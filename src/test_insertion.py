747af9fc2012b8569820cc02b3fa85db1904e82c
Zewei Sun <ge89jem@mytum.de>
2024-02-16 15:20:49 +0100

    1602

--- src/test_insertion.py
+++ src/test_insertion.py
@@ -199,42 +199,42 @@ def insertion():
             },
             "time_max": 10,
             "p0": {
-                "dX_d": [0.1, 0.5],
-                "ddX_d": [0.5, 1],
-                "DeltaX": [0, 0, 0, 0, 0, 0],
-                "K_x": [1500, 1500, 1500, 100, 100, 100]
+                "dX_d": [0.1, 0.5], # This is a list of length 2, indicating the target translation velocities of the robot end-effector in the X and Y directions during the insertion action. In this example, dX_d is [0.1, 0.5], indicating a target velocity of 0.1 meters per second in the X direction and 0.5 meters per second in the Y direction.
+                "ddX_d": [0.5, 1], # This is a list of length 2, indicating the target accelerations of the robot end-effector in the X and Y directions during the insertion action. In this example, ddX_d is [0.5, 1], indicating a target acceleration of 0.5 meters per second squared in the X direction and 1 meter per second squared in the Y direction.
+                "DeltaX": [0, 0, 0, 0, 0, 0], # This is a list of length 6, representing the corresponding displacement increments during the insertion action. In this example, DeltaX is [0, 0, 0, 0, 0, 0], indicating no displacement in the X, Y, and Z directions.
+                "K_x": [1500, 1500, 1500, 100, 100, 100] # This is a list of length 6, representing the control gains during the insertion action. In this example, K_x is [1500, 1500, 1500, 100, 100, 100], indicating control gains of 1500 in the X, Y, and Z directions and 100 in rotation directions. These gains are used to adjust the stability and accuracy of the robot during the insertion action.
             },
             "p1": {
-                "dX_d": [0.02, 0.05],
-                "ddX_d": [0.5, 0.1],
-                "K_x": [500, 500, 500, 100, 100, 100]
+                "dX_d": [0.02, 0.05], # This is a list of length 2, indicating the target translation velocities of the robot end-effector in the X and Y directions during the insertion action. Here, dX_d is [0.02, 0.05], meaning a target velocity of 0.02 meters per second in the X direction and 0.05 meters per second in the Y direction.
+                "ddX_d": [0.5, 0.1], # This is a list of length 2, indicating the target accelerations of the robot end-effector in the X and Y directions during the insertion action. In this example, ddX_d is [0.5, 0.1], indicating a target acceleration of 0.5 meters per second squared in the X direction and 0.1 meters per second squared in the Y direction.
+                "K_x": [500, 500, 500, 100, 100, 100] # This is a list of length 6, representing the control gains during the insertion action. Here, K_x is [500, 500, 500, 100, 100, 100], indicating control gains of 500 in the X, Y, and Z directions and 100 in rotation directions. These gains are used to adjust the stability and accuracy of the robot during the insertion action.
             },
             "p2": {
-                "search_a": [4, 6, 15, 0, 0, 0],
+                "search_a": [4, 6, 15, 0, 0, 0], # This is a list of length 6, representing the search amplitudes during the insertion action. It determines the range of motion or deviation allowed during the insertion process. For example, [4, 6, 15, 0, 0, 0] implies that there's a search amplitude of 4 in the X direction, 6 in the Y direction, and 15 in the Z direction, while no search amplitude is specified for rotation.
                 # "search_a": [5, 5, 0, 2, 2, 0],
-                "search_f": [1.2, 1.2, 0, 1.2, 1, 0],
+                "search_f": [1.2, 1.2, 0, 1.2, 1, 0], # This is a list of length 6, indicating the search frequencies during the insertion action. It determines how rapidly the system explores the search space. For instance, [1.2, 1.2, 0, 1.2, 1, 0] suggests a search frequency of 1.2 Hz in the X, Y, and Z directions, and 1 Hz for rotation, except for no search frequency specified in the Z rotation.
                 # "search_a": [0, 0, 0, 0, 0, 0],
                 # "search_f": [0, 0, 0, 0, 0, 0],
-                "search_phi": [0, math.pi/2, 0, 0, 0, 0],
-                "K_x": [1000, 1000, 1000, 100, 100, 100],
-                "f_push": [0, 0, 15, 0, 0, 0],
-                "dX_d": [0.1, 0.5],
-                "ddX_d": [0.5, 1]
+                "search_phi": [0, math.pi/2, 0, 0, 0, 0], # This is a list of length 6, representing the search phases during the insertion action. It determines the starting phase of the search motion. Here, [0, math.pi/2, 0, 0, 0, 0] indicates that the search starts from 0 radians in the X direction and π/2 radians (90 degrees) in the Y direction, while no phase shift is applied to other directions.
+                "K_x": [1000, 1000, 1000, 100, 100, 100], # This is a list of length 6, representing the control gains during the insertion action, similar to previous instances. It adjusts the stability and accuracy of the robot during insertion.
+                "f_push": [0, 0, 15, 0, 0, 0], # This is a list of length 6, specifying the pushing forces during the insertion action. It defines the magnitude of the force applied to the object being inserted. For example, [0, 0, 15, 0, 0, 0] indicates a force of 15 Newtons along the Z direction.
+                "dX_d": [0.1, 0.5], # This is a list of length 2, similar to previous instances, indicating the target translation velocities of the robot end-effector in the X and Y directions during the insertion action.
+                "ddX_d": [0.5, 1] # This is a list of length 2, similar to previous instances, indicating the target accelerations of the robot end-effector in the X and Y directions during the insertion action.
             },
             "p3": {
-                "dX_d": [0.1, 0.5],
-                "ddX_d": [0.5, 1],
-                "f_push": 10,
-                "K_x": [500, 500, 0, 800, 800, 800]
+                "dX_d": [0.1, 0.5], # This is a list of length 2, indicating the target translation velocities of the robot end-effector in the X and Y directions during the insertion action. It specifies the desired linear velocity for the insertion task. Here, [0.1, 0.5] suggests a target velocity of 0.1 meters per second in the X direction and 0.5 meters per second in the Y direction.
+                "ddX_d": [0.5, 1], # This is a list of length 2, indicating the target accelerations of the robot end-effector in the X and Y directions during the insertion action. It specifies the desired linear acceleration for the insertion task. For instance, [0.5, 1] implies a target acceleration of 0.5 meters per second squared in the X direction and 1 meter per second squared in the Y direction.
+                "f_push": 10, # This parameter indicates the pushing force during the insertion action. It defines the magnitude of the force applied by the robot end-effector while inserting the object. Here, f_push is set to 10, indicating a pushing force of 10 Newtons.
+                "K_x": [500, 500, 0, 800, 800, 800] # This is a list of length 6, representing the control gains during the insertion action. It adjusts the stability and accuracy of the robot during the insertion process. The values specified here affect the control behavior of the robot's end-effector. For example, [500, 500, 0, 800, 800, 800] suggests control gains of 500 in the X and Y directions, no gain in the Z direction, and 800 in rotation.
             }
         },
         "control": {
             "control_mode": 0
         },
         "user": {
-            "env_X": [0.01, 0.01, 0.002, 0.05, 0.05, 0.05],
-            "env_dX": [0.001, 0.001, 0.001, 0.005, 0.005, 0.005],
-            "F_ext_contact": [2.0, 1.0]
+            "env_X": [0.01, 0.01, 0.002, 0.05, 0.05, 0.05], # This parameter is a list of length 6, representing the environmental conditions related to position in the X, Y, and Z directions. It specifies the position tolerances or variations allowed in the environment. For example, [0.01, 0.01, 0.002, 0.05, 0.05, 0.05] indicates tolerance values of 0.01 meters in the X and Y directions, 0.002 meters in the Z direction, and 0.05 meters for rotation.
+            "env_dX": [0.001, 0.001, 0.001, 0.005, 0.005, 0.005], # This parameter is a list of length 6, similar to env_X, but it represents the environmental conditions related to velocity. It specifies the allowed velocity variations or tolerances in the environment. For instance, [0.001, 0.001, 0.001, 0.005, 0.005, 0.005] suggests velocity tolerance values of 0.001 meters per second in the X, Y, and Z directions, and 0.005 meters per second for rotation.
+            "F_ext_contact": [2.0, 1.0] # This parameter is a list of length 2, representing the external contact forces. It specifies the maximum forces allowed to be exerted on the objects or surfaces in contact with the robot during the insertion task. Here, [2.0, 1.0] indicates maximum contact forces of 2.0 Newtons in the X, Y, and Z directions and 1.0 Newtons for rotation.
         }
     }
     t = Task(robot)
